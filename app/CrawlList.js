a33e3533e01b1f758cbbd727396a5d7b7749069e
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-12-01 01:09:45 +0800

    西元2023年12月01日 (週五) 01時09分45秒 CST

--- app/CrawlList.js
+++ app/CrawlList.js
@@ -54,7 +54,7 @@ let ParseTable = async (outputArray = [], baseURL) => {
 
     // 將問題儲存到dc.title
     output['dc.title'] = eleTr.find('td:eq(1)').html()
-    output['dc.title'] = Tools.StripHTMLTagsss(output['dc.title'])
+    output['dc.title'] = Tools.StripHTMLTags(output['dc.title'])
     
 
     // 將日期儲存到dc.date
